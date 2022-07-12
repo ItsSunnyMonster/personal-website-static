8dcfc516f23c036ab7eeddbf0760f9b3c8d1ce0c
ItsSunnyMonster <100400733+ItsSunnyMonster@users.noreply.github.com>
2022-07-12 16:51:57 +1200

    Fixed mobile

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,9 @@
 const header = document.querySelector("[data-header]");
 const logo = document.querySelector("[data-logo]");
 const links = document.querySelectorAll("nav a");
+const hamMenu = document.querySelector("[data-nav-toggle-label]");
+const nav = document.querySelector("nav");
+const toggle = document.querySelector("#nav-toggle");
 
 const backgroundScroll = document.querySelector(
   "[data-background-scroll-detector]"
@@ -19,26 +22,33 @@ const logoObserver = new IntersectionObserver((entries, observer) => {
   });
 }, {});
 
-const backgroundObserver = new IntersectionObserver(
-  (entries, observer) => {
-    entries.forEach((entry) => {
-      if (entry.target == backgroundScroll) {
-        if (!entry.isIntersecting) {
-          header.classList.add("white");
-          links.forEach((link) => {
-            link.classList.add("white");
-          });
-        } else {
-          header.classList.remove("white");
-          links.forEach((link) => {
-            link.classList.remove("white");
-          });
-        }
+var options = { rootMargin: "-90px 0px 0px 0px" };
+
+const backgroundObserver = new IntersectionObserver((entries, observer) => {
+  entries.forEach((entry) => {
+    if (entry.target == backgroundScroll) {
+      if (!entry.isIntersecting) {
+        header.classList.add("white");
+        links.forEach((link) => {
+          link.classList.add("white");
+        });
+        hamMenu.classList.add("white");
+        nav.classList.add("white");
+      } else {
+        header.classList.remove("white");
+        links.forEach((link) => {
+          link.classList.remove("white");
+        });
+        hamMenu.classList.remove("white");
+        nav.classList.remove("white");
       }
-    });
-  },
-  { rootMargin: "-90px 0px 0px 0px" }
-);
+    }
+  });
+}, options);
+
+document.addEventListener("scroll", () => {
+  toggle.checked = false;
+});
 
 logoObserver.observe(logoScroll);
 backgroundObserver.observe(backgroundScroll);
