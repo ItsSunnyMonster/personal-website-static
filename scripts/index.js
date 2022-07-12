e0df008c7f5f0aa05f6e82a2811d58b7fc645567
ItsSunnyMonster <100400733+ItsSunnyMonster@users.noreply.github.com>
2022-07-12 16:36:40 +1200

    Changed animation a bit

--- scripts/index.js
+++ scripts/index.js
@@ -7,29 +7,38 @@ const backgroundScroll = document.querySelector(
 );
 const logoScroll = document.querySelector("[data-logo-scroll-detector]");
 
-const options = {
-  rootMargin: "-90px",
-};
-
-const observer = new IntersectionObserver((entries, observer) => {
+const logoObserver = new IntersectionObserver((entries, observer) => {
   entries.forEach((entry) => {
     if (entry.target == logoScroll) {
       if (!entry.isIntersecting) {
-        header.classList.add("white");
-        links.forEach((element) => {
-          element.classList.add("white");
-        });
         logo.classList.add("active");
       } else {
-        header.classList.remove("white");
-        links.forEach((element) => {
-          element.classList.remove("white");
-        });
         logo.classList.remove("active");
       }
     }
   });
-}, options);
+}, {});
 
-observer.observe(backgroundScroll);
-observer.observe(logoScroll);
+const backgroundObserver = new IntersectionObserver(
+  (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.target == backgroundScroll) {
+        if (!entry.isIntersecting) {
+          header.classList.add("white");
+          links.forEach((link) => {
+            link.classList.add("white");
+          });
+        } else {
+          header.classList.remove("white");
+          links.forEach((link) => {
+            link.classList.remove("white");
+          });
+        }
+      }
+    });
+  },
+  { rootMargin: "-90px 0px 0px 0px" }
+);
+
+logoObserver.observe(logoScroll);
+backgroundObserver.observe(backgroundScroll);
